4f615d7b5479e7ac119c56eaba6ef77b57672fe0
Dmitry <melkov.dmitriy40@gmail.com>
2025-03-10 12:23:56 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss';
+
+function App() {
+
+
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dolor ullam, obcaecati id est fugiat facere quo iure
+      soluta minima nesciunt sed temporibus similique assumenda excepturi alias nulla, quibusdam corrupti!
+    </div>
+  );
+}
+
+export default App;
